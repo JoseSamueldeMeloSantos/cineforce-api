61bce0a2000fd61b6567fc3c51ac7953e2f0f295
JoseSamueldeMeloSantos <josesamuelsantos321@hotmail.com>
2025-11-16 12:08:50 -0300

    EmailService

---
+++ src/main/java/br/com/bth8/cineforce/service/EmailService.java
@@ -0,0 +1,23 @@
+package br.com.bth8.cineforce.service;
+
+import br.com.bth8.cineforce.config.EmailConfig;
+import br.com.bth8.cineforce.mail.EmailSender;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+
+    @Autowired
+    private EmailSender sender;
+    @Autowired
+    private EmailConfig config;
+
+    public void sendSimpleEmail(
+            String to,String subject, String body) {
+        sender.to(to)
+                .withSubject(subject)
+                .withMessage(body)
+                .send(new EmailConfig());
+    }
+}
