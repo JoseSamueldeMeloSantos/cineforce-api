3c88f253648a251e208f5b7c49064c2bb521bedd
JoseSamueldeMeloSantos <josesamuelsantos321@hotmail.com>
2025-11-22 22:27:46 -0300

    passwordEncoder - SecurityConfig

---
+++ src/main/java/br/com/bth8/cineforce/config/SecurityConfig.java
@@ -0,0 +1,46 @@
+package br.com.bth8.cineforce.config;
+
+import br.com.bth8.cineforce.jwt.JwtTokenProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@EnableWebSecurity//Ativa as funcionalidades do Spring Security no projeto.
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final JwtTokenProvider tokenProvider;
+
+
+    @Bean
+    PasswordEncoder passwordEncoder() {//sse método define o padrão de criptografia da aplicação
+
+        // Cria um encoder PBKDF2 (forte e recomendado).
+        PasswordEncoder pbkdf2Encoder = new Pbkdf2PasswordEncoder(
+                "", 8, 185000,// vazio para gerar automaticamente  / comprimento da chave /numero de vezes que o algoritimo vai ser aplicado
+                Pbkdf2PasswordEncoder.SecretKeyFactoryAlgorithm.PBKDF2WithHmacSHA256//algoritimo de hash(criptografia)
+        );
+
+        // Mapa de encoders suportados. Ele permite que no futuro outros algoritmos sejam adicionados.
+        Map<String, PasswordEncoder> encoders = new HashMap<>();
+        encoders.put("pbkdf2", pbkdf2Encoder);// Registra o encoder PBKDF2 com o identificador "pbkdf2"
+
+
+                                                        //nome do algoritmo q a gente vai utilizar / hash de algoritimo q a gente vai usar
+        DelegatingPasswordEncoder passwordEncoder = new DelegatingPasswordEncoder("pbkdf2",encoders);
+
+        passwordEncoder.setDefaultPasswordEncoderForMatches(pbkdf2Encoder);// Define o encoder padrão para validação/codificação
+
+        return passwordEncoder;
+    }
+
+
+}
